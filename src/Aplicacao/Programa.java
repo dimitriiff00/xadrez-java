b5048c43e5470d1238b9852a2478c72494f68953
Dimitri Ribeiro <dimitriiff00@gmail.com>
2023-06-15 11:53:51 -0300

    primeira classe Posicao

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,10 +1,13 @@
 package Aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Posicao pos = new Posicao(3,5);
+		System.out.println(pos);
 	}
 
 }
